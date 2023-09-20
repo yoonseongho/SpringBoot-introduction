c113c905c447a5cd1c01c4620d6ed1744e27f6f6
SeeQ <sxdz1290@naver.com>
2023-09-20 15:28:17 +0900

    JPA commit

---
+++ src/main/java/spring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import spring.domain.Member;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
--- src/main/java/spring/springConfig.java
+++ src/main/java/spring/springConfig.java
@@ -5,30 +5,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.Service.MemberService;
 import spring.repository.JdbcMemberRepository;
-import spring.repository.JdbcTemplateMemberRepository;
 import spring.repository.MemberRepository;
-import spring.repository.MemoryMemberRepository;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class springConfig {
 
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
 
-    public springConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    public springConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcMemberRepository((DataSource) em);
+//    }
 }
